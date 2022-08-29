64d11ff63e074f0fac34abf575553fd767882162
Junyoung Son <junyoungson808@gmail.com>
2022-08-29 15:12:42 -0700

    lab 11 update

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 
 class BestBooks extends React.Component {
@@ -9,10 +10,35 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getBooks = async() => {
+    try {
+      let bookData = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+      this.setState({
+        books: bookData.data
+      })
+
+    } catch (error) {
+      console.log('we have an error ',)
+    }
+  
+  }
+
 
   render() {
 
     /* TODO: render all the books in a Carousel */
+
+    let carouselItems = this.state.photoData.map((pic, index) => (
+      <Carousel.Item key={index}>
+        <img
+          className="d-block w-100"
+          src={pic.src}
+          alt={pic.alt}
+        />
+        <Carousel.Caption>
+          <h3 style={{ backgroundColor: 'teal', borderRadius: '5px', width: 'max-content', margin: 'auto', padding: '5px' }}>Photo by: {pic.artist}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
 
     return (
       <>
