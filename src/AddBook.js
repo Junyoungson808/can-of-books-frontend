75c090d07fc543da85fcb2f5a869e1dd827f7ffc
Brandon Pitts <brandomoki.bp@gmail.com>
2022-08-30 19:04:21 -0700

    added delete button

---
+++ src/AddBook.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+class AddBook extends React.Component {
+
+
+  
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleBookCreate({
+      title: event.target.formTitle.value,
+      description: event.target.formDescription.value,
+      status: event.target.formStatus.checked,
+    })
+  }
+
+  render() {
+
+
+return (
+
+    <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formTitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="name" placeholder="Enter book title" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="name" placeholder="Enter book description" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formStatus">
+            <Form.Check type="checkbox" label="Check availability" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+
+)
+
+  }
+  
+}
+
+export default AddBook;
